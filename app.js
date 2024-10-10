7f01dc617e5afdf7c633f4f5f8b9ef576a8cee3a
PTParra <PatricioTristante1999@gmail.com>
2024-10-10 16:09:38 +0200

    commit inicial

---
+++ app.js
@@ -0,0 +1,10 @@
+/*
+    Using array methods, you will transform an array of strings into a secret message!
+
+    You should consult the Mozilla Developer Network (MDN) for reference on any method with 
+    which you are not familiar.
+*/
+
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the',
+    'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 
+    'Chris', 'Pine,', 'Learn', 'JavaScript'];
